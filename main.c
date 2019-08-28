584ff37f7bbdc98d3c1244de6b370cc54615fded
Regler <46320305+Regler@users.noreply.github.com>
2019-08-28 18:29:03 +0800

    修改过滤条件 能实现简单的ip分片 但是有缺陷 需要改善

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <pcap.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>  
 #include <sys/types.h>  
 #include <dirent.h>  
@@ -15,10 +16,16 @@
 #include <pcap/pcap.h>
 #include "list.h"
 #define LINE_LEN 16
-u_char *data;
-int length;
 int mtu;
 pcap_t *fp;
+#define IP_HEAD 20
+#define MAC_HEAD 14
+
+typedef struct node
+{
+	struct pcap_pkthdr *hdr;
+	u_char *data;
+}Packet;
 
 
 pcap_t *source_pcap_t=NULL;
@@ -27,22 +34,92 @@ MyList *list=NULL;
 
 
 
+
+
 void dispatcher_handler(u_char *temp1, const struct pcap_pkthdr *header, const u_char *pkt_data)
 {
-	u_int i=0;
-	
-	//打印出数据包的数据
-	length = header->caplen;
-	data = (u_char *) malloc (sizeof(u_char) * length + 1);
-	for (i=0; (i < header->caplen) ; i++)
-	{
-		printf("%.2x ", pkt_data[i]);
-		data[i]=pkt_data[i];
-		if ( (i % LINE_LEN) == 15) printf("\n");
+		
+	int num_ip_fragment = (header->len - MAC_HEAD - IP_HEAD)/(mtu - IP_HEAD) + 1;
+	printf("num_ip_fragment = %d\n", num_ip_fragment);
 
+	const u_char *pdata =pkt_data + MAC_HEAD + IP_HEAD; 
+		
+	for(int i=1;i<=num_ip_fragment;i++)
+	{
+		Packet *packet = (Packet *)malloc(sizeof(Packet));
+		if(i != num_ip_fragment)
+		{
+			packet->data = (u_char *) malloc (sizeof(u_char) *(MAC_HEAD + mtu) + 1);
+			packet->hdr = (struct pcap_pkthdr *)malloc(sizeof(struct pcap_pkthdr));
+
+			packet->hdr->len = mtu + MAC_HEAD;
+			packet->hdr->caplen =  mtu + MAC_HEAD;
+			packet->hdr->ts.tv_sec = header->ts.tv_sec;
+			packet->hdr->ts.tv_usec = header->ts.tv_usec;
+				
+			int j;
+			int index= 0 ;
+
+			for (j = 0; j < MAC_HEAD + IP_HEAD; j++)
+				packet->data[index++] = pkt_data[j];
+
+			u_char *fflag = packet->data + 14 + 6;
+			fflag[0]^= (1<<5);
+			u_char *flength = packet->data + 14 + 3;
+			flength[0] = (u_char) mtu;
+
+			u_char *foffset= packet->data + 14 + 7;
+			int offset = (i-1)*(mtu-IP_HEAD)/8;
+			printf("%d     offset \n", offset);
+			foffset[0] =  (u_char) offset;
+
+			for (j = (i-1)*(mtu-IP_HEAD); j <i*(mtu - IP_HEAD); j++ )
+				packet->data[index++] = pdata[j];
+			packet->data[index]='\0';
+
+			myListInsertDataAtLast(list, packet); 
+		}
+
+		else 
+		{
+			packet->data = (u_char *)malloc(header->len - (mtu - IP_HEAD)*(num_ip_fragment -1) + 1);
+			packet->hdr = (struct pcap_pkthdr *)malloc(sizeof(struct pcap_pkthdr));
+
+			packet->hdr->len = header->len - (mtu - IP_HEAD)*(num_ip_fragment - 1 );
+
+			printf ("end   length  %d  \n", packet -> hdr ->len );
+			packet->hdr->caplen =  packet->hdr->len;
+			packet->hdr->ts.tv_sec = header->ts.tv_sec;
+			packet->hdr->ts.tv_usec = header->ts.tv_usec;
+
+			int j;
+			int index= 0 ;
+
+			for (j = 0; j < MAC_HEAD + IP_HEAD; j++)
+				packet->data[index++] = pkt_data[j];
+
+			u_char *fflag= packet->data + 14 + 6;
+			fflag[0]^= (0<<5);
+
+			int offset = (i-1)*(mtu-IP_HEAD)/8;	
+			printf("%d     offset \n", offset);
+
+			u_char *foffset= packet->data + 14 + 7;
+			foffset[0]=(u_char) offset;
+	
+			u_char *flength = packet->data + 14 + 3;
+			flength[0] = (u_char) packet->hdr->len - MAC_HEAD;
+
+			for (j = (num_ip_fragment - 1)*(mtu-IP_HEAD); j < header->len - MAC_HEAD - IP_HEAD; j++ )
+				packet->data[index++] = pdata[j];
+
+			packet->data[index]='\0';
+
+			myListInsertDataAtLast(list, packet); 
+
+		}
 	}
-	myListInsertDataAtLast(list, data); 
-	printf("\n\n");		
+
 
 }
 
@@ -62,6 +139,10 @@ void print_pcap(char *file)
 	pcap_close(fp);
 
 }
+
+
+
+
 
 
 int exit_main()
@@ -93,7 +174,9 @@ void filter_pcap(char *file)
 		exit_main();		
 	}
 	struct bpf_program filter;
-	if( -1==pcap_compile(source_pcap_t, &filter, "icmp", 1, 0) )
+	char filter_str[20];
+	snprintf(filter_str,sizeof(filter_str),"greater %d",mtu);
+	if( -1==pcap_compile(source_pcap_t, &filter, filter_str, 1, 0) )
 	{
 		printf("pcap_compile() fail.\n");
 		printf("errno:%s\n", pcap_geterr(source_pcap_t));
@@ -135,7 +218,7 @@ void filter_pcap(char *file)
 
 
 
-void write_pcap( u_char *packet_data)
+void write_pcap()
 {
 	pcap_dumper_t *pdumper;
 	char errbuf[PCAP_ERRBUF_SIZE]={0};
@@ -151,15 +234,15 @@ void write_pcap( u_char *packet_data)
 		exit_main();		
 	}
 
-	struct pcap_pkthdr *hdr;
-	hdr = (struct pcap_pkthdr *)malloc(sizeof(struct pcap_pkthdr));
-	hdr->caplen = length;
-	hdr->len = length;
-
-	pcap_dump((u_char*)pdumper, hdr, packet_data);
-	data[33] = 0x22; 
-	pcap_dump((u_char*)pdumper, hdr, packet_data);
-
+	
+	MyNode *p = list ->first;
+	while(p)
+	{
+		Packet *packet = (Packet *)p->data;
+		pcap_dump((u_char*)pdumper, packet->hdr, packet->data);
+		p=p->next;
+	}
+	
 
 	pcap_dump_flush(pdumper);
 	pcap_dump_close(pdumper);
@@ -169,7 +252,12 @@ void write_pcap( u_char *packet_data)
 
 
 
-
+void free_data(void *data)
+{
+	Packet *packet = (Packet *)data;
+	free(packet->hdr);
+	free(packet->data);
+}
 
 
 
@@ -199,10 +287,10 @@ int main(int argc, char *argv[])
 		filter_pcap(argv[i]);
 		list = createMyList();
 		print_pcap("gl_icmp.pcap");	
-		write_pcap(data);
+		write_pcap();
 	}
 	printf("\n%d \n", list->length);
-
+	freeMyList(list,free_data);
 
 	return 0;
 }
